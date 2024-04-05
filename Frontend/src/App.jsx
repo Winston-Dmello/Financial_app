86a5a7b4d3711ce39be55f581352fde0ef0b1d82
= <=>
2024-04-06 05:28:09 +0530

    fixed gits stupid problems

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import { Route, Routes, Navigate } from "react-router-dom";
-<<<<<<< HEAD
+
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
@@ -10,17 +10,8 @@ import UserProfile from "./Components/User_profile";
 import EditProfile from "./Components/Edit_Profile";
 import Categories from "./Components/Categories";
 import File from "./Components/File";
-
-=======
-import Signup from './Components/Signup'
-import Login from './Components/Login'
-import Home from './Components/Home'
-import About from './Components/About';
-import UserProfile from './Components/User_profile';
-import EditProfile from './Components/Edit_Profile';
-import Categories from './Components/Categories';
 import Analysis from './Components/Analysis';
->>>>>>> 45a9444cb2ac636745124247dbd024347fdb5c0b
+
 function App() {
 
   return (
@@ -31,17 +22,11 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
-<<<<<<< HEAD
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/edit_profile" element={<EditProfile />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/file" element={<File />} />
-=======
-        <Route path="/profile" element={<UserProfile/> }/>
-        <Route path="/edit_profile" element={<EditProfile/> }/>
-        <Route path="/categories" element={<Categories/> }/>
         <Route path="/analysis" element={<Analysis/> }/>
->>>>>>> 45a9444cb2ac636745124247dbd024347fdb5c0b
       </Routes>
     </>
   )
--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
 
+  [transactions, setTransactions] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async (userId = "") => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/${userId}/Get_Transaction`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          console.log("data", data);
+          setTransactions(data);
+        } else {
+          alert("Something went wrong Try Again Later!");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  }, []);
   const handleSignUp = () => {
     navigate("/signup");
   };
@@ -19,14 +39,10 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to Our Fintech App</h1>
-      <p>
-        Under Construction, Please come back again
-      </p>
+      <p>Under Construction, Please come back again</p>
       <button onClick={handleSignUp}>Sign Up</button>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleAbout}>About</button>
     </div>
   );
 }
-
-
