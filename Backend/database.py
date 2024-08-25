c5105df790ee739cb4019e753987afed2b2070cd
Nandainthegrass <postnanda200@gmail.com>
2024-08-25 10:55:06 +0530

    db

--- Backend/database.py
+++ Backend/database.py
@@ -1,6 +1,6 @@
 import motor.motor_asyncio
 
-MONGO_DETAILS = "mongodb+srv://Winston:Winston2004@fintard.yniryxo.mongodb.net/"
+MONGO_DETAILS = "mongodb://localhost:27017/"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
 database = client["Fintard"]
